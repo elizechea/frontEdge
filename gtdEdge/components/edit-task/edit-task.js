05f145033070024d38e3baa7f026554a0a0032cc
Alberto Basalo <albertobasalo@hotmail.com>
2015-12-08 20:16:10 +0100

    style states

--- gtdEdge/components/edit-task/edit-task.js
+++ gtdEdge/components/edit-task/edit-task.js
@@ -6,11 +6,12 @@ let name = 'editTask'
 ng.module(name, ['ngMaterial'])
 	.controller ('EditTaskController',EditTaskController)
 
-function EditTaskController($routeParams,tasksDataService) {
+function EditTaskController($routeParams,$router, tasksDataService) {
 			let taskId = $routeParams.id
 			this.task = tasksDataService.findTask(taskId)
 			this.saveTask = ()=>{
 				tasksDataService.updateTask(taskId)
+				$router.parent.navigate('/')
 			}
 		}
 
