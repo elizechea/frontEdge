3cb0476e5a1645dac1d7e4a617c63c189c54c58c
Alberto Basalo <albertobasalo@hotmail.com>
2015-12-14 09:55:09 +0100

    edit task

--- 3-gtd-edge/components/task/task.js
+++ 3-gtd-edge/components/task/task.js
@@ -7,8 +7,17 @@ let name = 'task'
 ng.module(name, ['ngMaterial'])
 	.controller('TaskController', TaskController)
 
-function TaskController(tasksDataService) {
-	
+function TaskController($routeParams, $router,tasksDataService) {
+	let taskId = $routeParams.id
+	this.task = tasksDataService.findTask(taskId)
+	this.saveTask = () => {
+		tasksDataService.updateTask(taskId)
+		$router.parent.navigate('/')
+	}
+	this.deleteTask = () => {
+		tasksDataService.deleteTask(taskId)
+		$router.parent.navigate('/')
+	}
 }
 
 export default name
